ba0333929257c7a09ce843230245d0f31e973069
ninico11 <112957880+ninico11@users.noreply.github.com>
2023-10-18 10:04:55 +0300

    update: added method getpatient and recoverd initial metod gettreatment

--- MedTracker/Controllers/DoctorController.cs
+++ MedTracker/Controllers/DoctorController.cs
@@ -19,10 +19,99 @@ public class DoctorController : ControllerBase
     {
         _context = context;
     }
-
     [HttpGet("GetTreatments")]
     [Authorize]
     public async Task<IActionResult> GetTreatments()
+    {
+        int currentUserId = 0;
+        try
+        {
+            var claimUserId = User.Claims.First(claim => claim.Type == "userId").Value;
+            currentUserId = Convert.ToInt32(claimUserId);
+
+            var user = await _context.Users
+                .Where(u => u.IdUser == currentUserId)
+                .ToListAsync();
+            if (!(user.Count == 1 && user[0].IsDoctor))
+                return BadRequest(new { Message = "The user is not a doctor." });
+        }
+        catch
+        {
+            return BadRequest(new { Message = "No user id was found, check the token." });
+        }
+
+        try
+        {
+           // Fetch treatments associated with the current patient
+           var treatments = await _context.Treatments
+               .Where(t => t.DoctorID == currentUserId)
+               .Select(t => new
+               {
+                   t.IdTreatment,
+                   t.TName,
+                   t.StatusTreatment,
+                   t.Start_Time,
+                   t.End_Time,
+                   t.NotePatient,
+                   t.NoteDoctor,
+                   t.DoctorID,
+               })
+               .ToListAsync();
+
+            var treatmentsIds = new List<int>();
+            foreach (var treatment in treatments)
+            {
+                treatmentsIds.Add(treatment.IdTreatment);
+            }
+
+            var medications = await _context.Treatment_Medication
+                .Where(tm => treatmentsIds.Contains(tm.IdTreatment))
+                .Join(
+                    _context.Medications,
+                    tm => tm.IdMedication,
+                    m => m.IdMedication,
+                    (tm, m) => new
+                    {
+                        tm.IdTreatment,
+                        m.IdMedication,
+                        m.PName,
+                        m.MDescription,
+                        m.Start_Time,
+                        m.End_Time,
+                        m.TimeUse,
+                        m.Quantity
+                    })
+                .ToListAsync();
+
+            var treatmentsWithMedications = treatments
+            .GroupJoin(
+                medications,
+                t => t.IdTreatment,
+                m => m.IdTreatment,
+                (t, meds) => new
+                {
+                    t.IdTreatment,
+                    t.TName,
+                    t.StatusTreatment,
+                    t.Start_Time,
+                    t.End_Time,
+                    t.NotePatient,
+                    t.NoteDoctor,
+                    t.DoctorID,
+                    Medications = meds.ToList()
+                })
+            .ToList();
+
+            return Ok(treatmentsWithMedications);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred:{ex.Message}");
+        }
+    }
+    [HttpGet("GetPatient")]
+    [Authorize]
+    public async Task<IActionResult> GetPateint()
     {
         int currentUserId = 0;
         try
@@ -49,11 +138,6 @@ public class DoctorController : ControllerBase
                 {
                     t.IdTreatment,
                     t.TName,
-                    t.StatusTreatment,
-                    t.Start_Time,
-                    t.End_Time,
-                    t.NotePatient,
-                    t.NoteDoctor,
                     t.DoctorID,
                 })
                 .ToListAsync();
@@ -76,51 +160,26 @@ public class DoctorController : ControllerBase
                     })
                 .ToListAsync();
 
-            var medications = await _context.Treatment_Medication
-                .Where(tm => treatmentsIds.Contains(tm.IdTreatment))
-                .Join(
-                    _context.Medications,
-                    tm => tm.IdMedication,
-                    m => m.IdMedication,
-                    (tm, m) => new
-                    {
-                        tm.IdTreatment,
-                        m.IdMedication,
-                        m.PName,
-                        m.MDescription,
-                        m.Start_Time,
-                        m.End_Time,
-                        m.TimeUse,
-                        m.Quantity
-                    })
-                .ToListAsync();
 
-            var treatmentsWithPatientsAndMedications = new List<object>();
+            var treatmentsWithPatients = new List<object>();
 
             foreach (var treatment in treatments)
             {
                 var treatmentId = treatment.IdTreatment;
                 var patient = patients.FirstOrDefault(p => p.IdTreatment == treatmentId);
-                var meds = medications.Where(m => m.IdTreatment == treatmentId).ToList();
 
-                var treatmentWithPatientAndMedications = new
+                var treatmentWithPatient = new
                 {
                     treatment.IdTreatment,
                     treatment.TName,
-                    treatment.StatusTreatment,
-                    treatment.Start_Time,
-                    treatment.End_Time,
-                    treatment.NotePatient,
-                    treatment.NoteDoctor,
                     treatment.DoctorID,
-                    Patient = patient,
-                    Medications = meds
+                    Patient = patient
                 };
 
-                treatmentsWithPatientsAndMedications.Add(treatmentWithPatientAndMedications);
+                treatmentsWithPatients.Add(treatmentWithPatient);
             }
 
-            return Ok(treatmentsWithPatientsAndMedications);
+            return Ok(treatmentsWithPatients);
         }
         catch (Exception ex)
         {
